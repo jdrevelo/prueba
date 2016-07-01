1a08f5dec3685a767722e6bca28ebd2110f4a6d0
Levya <jdrevelo9@misena.edu.co>
2016-07-01 15:30:29 -0500

    se modifico el titulo y el nombre de la pestaña

--- Desktop/GitPru/practicagit.php
+++ Desktop/GitPru/practicagit.php
@@ -1,8 +1,11 @@
 <html>
   <head>
+  <title>Practica de Git con GitHub</title>
   </head>
     <body background="https://k60.kn3.net/taringa/B/2/4/7/F/3/Goku-Master/04D.jpg">
-	<title>Practica de Git con GitHub</title>
+	  <center>
+	    <h1>Practica de Git con GitHub</h1>
+	  </center>
 	<?php
 	echo "Hola Mundo";
 	?>
