5b4ef30beb630c73e14eda11b9953f481024eaed
Levya <jdrevelo9@misena.edu.co>
2016-07-01 15:15:47 -0500

    se cambio la imagen del archivo

--- Desktop/GitPru/practicagit.php
+++ Desktop/GitPru/practicagit.php
@@ -1,7 +1,7 @@
 <html>
   <head>
   </head>
-    <body background="gokuSombra.png" style="background-repeat:no-repeat">
+    <body background="https://k60.kn3.net/taringa/B/2/4/7/F/3/Goku-Master/04D.jpg">
 	<title>Practica de Git con GitHub</title>
 	<?php
 	echo "Hola Mundo";
