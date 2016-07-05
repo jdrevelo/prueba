13261fec106deb00b4506eaa4b9c1c0c12d99b4f
Levya <jdrevelo9@misena.edu.co>
2016-07-05 11:02:59 -0500

    Se agrego etiqueta 'meta' para las acentuacionesy la letra 'ñ'

--- Desktop/GitPru/practicagit.php
+++ Desktop/GitPru/practicagit.php
@@ -1,6 +1,7 @@
 <html>
   <head>
   <title>Practica de Git con GitHub</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   </head>
     <body background="https://k60.kn3.net/taringa/B/2/4/7/F/3/Goku-Master/04D.jpg">
 	  <center>
