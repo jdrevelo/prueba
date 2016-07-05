7bc7e332b8c65fb647877a5643fc93b08abd4a69
Levya <jdrevelo9@misena.edu.co>
2016-07-05 10:20:47 -0500

    se modifico el color de las letras

--- Desktop/GitPru/practicagit.php
+++ Desktop/GitPru/practicagit.php
@@ -4,35 +4,35 @@
   </head>
     <body background="https://k60.kn3.net/taringa/B/2/4/7/F/3/Goku-Master/04D.jpg">
 	  <center>
-	    <h1>Practica de Git con GitHub</h1>
+	    <font color="white"><h1>Practica de Git con GitHub</h1>
 	  </center>
 	  <p>
 	  Git (pronunciado "guit" ) es un software de control de versiones diseñado por Linus Torvalds,
 	  pensando en la eficiencia y la confiabilidad del mantenimiento de versiones de aplicaciones 
 	  cuando éstas tienen un gran número de archivos de código fuente.
-	  </p>
-    <table>
+	  </font></p>
+    <table width="50%" border="3" bordercolor="#green" cellspacing="10" cellpadding="10">
 	    <thead>
 		  <tr>
-		    <th>
+		    <th><font color="white">
 			  lenguajes
-			</th>
+	   	    </font></th>
 		  </tr>
 		</thead>
 	  <tr>
-	    <td>
+	    <td><font color="white">
 		  PHP
-		</td>
+		</font></td>
 	  </tr>
 	  <tr>
-	    <td>
+	    <td><font color="white">
 		  HTML
-		</td>
+		</font></td>
 	  </tr>
 	  <tr>
-	    <td>
+	    <td><font color="white">
 		  JAVA
-		</td>
+		</font></td>
 	  </tr>
     </table>	
 	<?php
